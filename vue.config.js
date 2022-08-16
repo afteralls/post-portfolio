47efe9517b5dc3db00935cb6e935aa2184950a64
Post-Apocalypse <postapocalypsecore@gmail.com>
2022-08-16 19:26:01 +0600

    manifest settings update

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    name: 'Резюме',
+    short_name: 'post-portfolio',
+    themeColor: '#252423',
+    lang: 'ru',
+    manifestOptions: {
+      display: 'standalone',
+      background_color: '#252423'
+    }
+  }
 })
