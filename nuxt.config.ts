b7df75ae44594354d1084de25e0200f20087c0a3
Daniil Sukharnikov <afterallspace@gmail.com>
2024-02-17 00:35:03 +0600

    app: refactoring & add extends

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  extends: ['github:afteralls/ui'],
+  imports: {
+    dirs: ['./constants']
+  },
+  i18n: {
+    langDir: './locale/lang',
+    locales: [
+      { code: 'en', name: 'English', file: 'en.ts' },
+      { code: 'ru', name: 'Русский', file: 'ru.ts' }
+    ]
+  },
+  app: {
+    head: { title: 'Portfolio' }
+  }
 })
